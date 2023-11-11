6105e9c27c92e19d5af2ecf3dc26ec8942190137
EZ_laptop <d1ablo@ua.fm>
2023-11-11 15:59:47 +0200

    Updating FoodGraber logic

--- Assets/Scripts/FoodGenerator/FoodGenerator.cs
+++ Assets/Scripts/FoodGenerator/FoodGenerator.cs
@@ -7,13 +7,18 @@ public class FoodGenerator : MonoBehaviour
     public GameObject FoodToGenerate;
     public GameObject SpotToSpawn;
     public float SpawnRate = 1;
-    public float SpawnGap = 5;
-    public int MaxFoodAmount = 3;
+    //float SpawnGap = 5;
+    int MaxFoodAmount = 4;
     int CurrentFoodAmount = 0;
+	public Vector2[] SpotsArray;
+	List<GameObject> FoodArray;
+
+	//TODO: Handle array of created Food, and select an empty spot that are free after Retrieving food
 
     // Start is called before the first frame update
     void Start()
     {
+		FoodArray = new FoodArray[4];
         StartFoodGeneration();
     }
 
@@ -32,12 +37,44 @@ public class FoodGenerator : MonoBehaviour
     {
         if (CurrentFoodAmount < MaxFoodAmount)
         {
-            Vector3 SpawnPos = SpotToSpawn.transform.position;
-            SpawnPos.y += CurrentFoodAmount * SpawnGap;
-
-            Instantiate(FoodToGenerate, SpawnPos, Quaternion.Euler(-90, 0, 0));
-            CurrentFoodAmount++;
-            StartFoodGeneration();
+			Vector3 SpawnPos = SpotToSpawn.transform.position;
+			SpawnPos.x += SpotsArray[CurrentFoodAmount].x;
+			SpawnPos.z += SpotsArray[CurrentFoodAmount].y;
+	
+			GameObject Food = Instantiate(FoodToGenerate, SpawnPos, Quaternion.Euler(0, 0, 0));
+			FoodArray.Add(Food);
+			CurrentFoodAmount++;
+			StartFoodGeneration();
         }
     }
+	
+	int GetEmptySlotIndex()
+	{
+		for(int i = 0; i < MaxFoodAmount; i++)
+		{
+			if(FoodArray[i] == null)
+			{
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public int GetCurrentFoodAmount()
+	{
+		return CurrentFoodAmount;
+	}
+	
+	public GameObject RetrieveFood()
+	{
+		if(FoodArray.Num() > 0)
+		{
+			GameObject Food = FoodArray[CurrentFoodAmount];
+			FoodArray.RemoveAt(CurrentFoodAmount);
+			CurrentFoodAmount--;
+			return Food;
+		}
+		return null;
+	}
 }
--- Assets/Scripts/FoodGenerator/FoodGrabber.cs
+++ Assets/Scripts/FoodGenerator/FoodGrabber.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class FoodGrabber : MonoBehaviour
 {
+	public GameObject FoodHolderSpot;
+	
+	int CurrentFoodHolded = 0;
+	int MaxFoodHolded = 5;
+	FoodGenerator CurrentFG;
+	GameObject CurrentFoodObj;
+	bool bRetrieveAnimStarted = false;
+	public float RetrieveAnimDuration = 1;
+	float CurrentRetrieveAnimTime = 0;
+	Vector3 StartPosition;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -13,16 +24,56 @@ public class FoodGrabber : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+		if(bRetrieveAnimStarted)
+		{
+			float Alpha = 1 / RetrieveAnimDuration * Time.deltaTime;
+			print("Alpha " + Alpha);
+			Vector3 interpolatedPosition = Vector3.Lerp(StartPosition, FoodHolderSpot.transform.position, Alpha);
+			CurrentRetrieveAnimTime += Time.deltaTime;
+			print("Anim time " + CurrentRetrieveAnimTime);
+			CurrentFoodObj.transform.position = interpolatedPosition;
+			
+			if(CurrentRetrieveAnimTime >= RetrieveAnimDuration)
+			{
+				bRetrieveAnimStarted = false;
+			}
+		}
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        print("Trigger enter " + other.name);
+		if(other.tag == "FoodGenerator")
+		{
+			print("Trigger enter " + other.name);
+			CurrentFG = other.GetComponent<FoodGenerator>();
+			if(CurrentFG.GetCurrentFoodAmount() > 0 && CanGrabMoreFood())
+			{
+				TryGrabFood();
+			}
+		}
+        
     }
 
     private void OnTriggerExit(Collider other)
     {
-        print("Trigger exit " + other.name);
+		if(other.tag == "FoodGenerator")
+		{
+			print("Trigger exit " + other.name);
+		}
     }
+	
+	bool CanGrabMoreFood()
+	{
+		return CurrentFoodHolded < MaxFoodHolded;
+	}
+	
+	void TryGrabFood()
+	{
+		CurrentFoodObj = CurrentFG.RetrieveFood();
+		if(CurrentFoodObj != null)
+		{
+			bRetrieveAnimStarted = true;
+			StartPosition = CurrentFoodObj.transform.position;
+		}
+	}
 }
